012cbb5ccd7e089d66cdcea22ed8b5423784938f
Alex Izotov <martinandrudy@gmail.com>
2014-09-03 17:49:26 +0300

    added some tests for static pages and changed site style with sass
    Signed-off-by: Alex Izotov <martinandrudy@gmail.com>

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,21 +8,24 @@ describe "Static pages" do
     before { visit root_path }
 
     it { should have_title("Library") }
-    it { should have_content("Welcome to the Library App") }
+    it { should have_selector('h1', text: "Welcome to the Library App") }
   end
 
   describe "About" do
     before { visit about_path }
     it { should have_title("About Us | Library") }
+    it { should have_selector('h1', text: "About Us") }
   end
 
   describe "Contacts" do
     before { visit contacts_path }
     it { should have_title("Contacts | Library") }
+    it { should have_selector('h1', text: "Contacts") }
   end
 
   describe "Help" do
     before { visit help_path }
     it { should have_title("Help | Library") }
+    it { should have_selector('h1', text: "Help") }
   end
 end
