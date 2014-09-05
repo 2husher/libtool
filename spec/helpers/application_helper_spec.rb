d29ef40559aa0647e44aafbc0b16d7513f4107b8
Alex Izotov <martinandrudy@gmail.com>
2014-09-06 00:29:05 +0300

    added full_title test and deleted duplicated full_title helper
    Signed-off-by: Alex Izotov <martinandrudy@gmail.com>

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the StaticPagesHelper. For example:
+#
+# describe StaticPagesHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       helper.concat_strings("this","that").should == "this that"
+#     end
+#   end
+require 'spec_helper'
+
+describe ApplicationHelper do
+
+  describe "full_title" do
+    it "should include the page title" do
+      expect(full_title("foo")).to match(/foo/)
+    end
+
+    it "should include the base title" do
+      expect(full_title("")).to match(/Library\z/)
+    end
+
+    it "should not include a bar for the home page" do
+      expect(full_title("")).not_to match(/\|/)
+    end
+  end
+end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,7 +1,1 @@
-def full_title(page_title)
-    if page_title.empty?
-        "Library"
-    else
-        "#{page_title} | Library"
-    end
-end
+include ApplicationHelper
